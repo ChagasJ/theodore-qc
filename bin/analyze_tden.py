a633584616be7483223c03941e148fdef655dfec
Felix Plasser (lin) <f.plasser@lboro.ac.uk>
2021-04-22 10:00:46 +0100

    call via library, #6

--- bin/analyze_tden.py
+++ bin/analyze_tden.py
@@ -17,8 +17,6 @@ except ImportError:
 
 from theodore import theo_header, lib_tden, lib_exciton, input_options, error_handler
 
-(tc, tt) = (process_time(), perf_counter())
-
 def ihelp():
     print(" analyze_tden.py")
     print(" Command line options:")
@@ -30,59 +28,65 @@ def ihelp():
 # Parsing and computations
 #--------------------------------------------------------------------------#
 
-ifile = 'dens_ana.in'
+def main(largv):
+    (tc, tt) = (process_time(), perf_counter())
 
-arg=sys.argv.pop(0)
-while len(sys.argv)>0:
-    arg = sys.argv.pop(0)
-    if arg in ["-h", "-H", "--help"]:
+    ifile = 'dens_ana.in'
+
+    arg=largv.pop(0)
+    while len(largv)>0:
+        arg = largv.pop(0)
+        if arg in ["-h", "-H", "--help"]:
+            ihelp()
+        elif arg == '-ifile' or arg == '-f':
+            ifile = largv.pop(0)
+        else:
+            raise error_handler.ElseError(arg, 'command line option')
+
+    if not os.path.exists(ifile):
+        print('Input file %s not found!'%ifile)
+        print('Please create this file using theoinp or specify its location using -ifile\n')
         ihelp()
-    elif arg == '-ifile' or arg == '-f':
-        ifile = sys.argv.pop(0)
-    else:
-        raise error_handler.ElseError(arg, 'command line option')
 
-if not os.path.exists(ifile):
-    print('Input file %s not found!'%ifile)
-    print('Please create this file using theoinp or specify its location using -ifile\n')
-    ihelp()
+    ioptions = input_options.tden_ana_options(ifile)
+    theo_header.print_header('Transition density matrix analysis', ioptions=ioptions)
 
-ioptions = input_options.tden_ana_options(ifile)
-theo_header.print_header('Transition density matrix analysis', ioptions=ioptions)
+    tdena = lib_tden.tden_ana(ioptions)
+    if 'mo_file' in ioptions: tdena.read_mos()
 
-tdena = lib_tden.tden_ana(ioptions)
-if 'mo_file' in ioptions: tdena.read_mos()
+    tdena.read_dens()
 
-tdena.read_dens()
+    if 'at_lists' in ioptions or ioptions['eh_pop'] >= 1:
+        tdena.compute_all_OmAt()
 
-if 'at_lists' in ioptions or ioptions['eh_pop'] >= 1:
-    tdena.compute_all_OmAt()
+    if 'at_lists' in ioptions:
+        tdena.compute_all_OmFrag()
+        if ioptions['print_OmFrag']: tdena.fprint_OmFrag()
 
-if 'at_lists' in ioptions:
-    tdena.compute_all_OmFrag()
-    if ioptions['print_OmFrag']: tdena.fprint_OmFrag()
+    if ioptions['comp_ntos']:  tdena.compute_all_NTO()
+    if ioptions['comp_dntos']: tdena.compute_all_DNTO()
+    if ioptions['comp_p_h_dens']: tdena.compute_p_h_dens()
+    if ioptions['comp_rho0n']: tdena.compute_rho_0_n()
 
-if ioptions['comp_ntos']:  tdena.compute_all_NTO()
-if ioptions['comp_dntos']: tdena.compute_all_DNTO()
-if ioptions['comp_p_h_dens']: tdena.compute_p_h_dens()
-if ioptions['comp_rho0n']: tdena.compute_rho_0_n()
+    if 'RMSeh' in ioptions.get('prop_list') or 'MAeh' in ioptions.get('prop_list') or 'Eb' in ioptions.get('prop_list'):
+        exca = lib_exciton.exciton_analysis()
+        exca.get_distance_matrix(tdena.struc)
+        tdena.analyze_excitons(exca)
 
-if 'RMSeh' in ioptions.get('prop_list') or 'MAeh' in ioptions.get('prop_list') or 'Eb' in ioptions.get('prop_list'):
-    exca = lib_exciton.exciton_analysis()
-    exca.get_distance_matrix(tdena.struc)
-    tdena.analyze_excitons(exca)
+    if 'Phe' in ioptions['prop_list']:
+        tdena.compute_all_Phe()
 
-if 'Phe' in ioptions['prop_list']:
-    tdena.compute_all_Phe()
+    #--------------------------------------------------------------------------#
+    # Print-out
+    #--------------------------------------------------------------------------#
 
-#--------------------------------------------------------------------------#
-# Print-out
-#--------------------------------------------------------------------------#
+    tdena.print_all_eh_pop()
 
-tdena.print_all_eh_pop()
+    tdena.print_summary()
 
-tdena.print_summary()
+    #print 'Finished at ' + time.asctime()
 
-#print 'Finished at ' + time.asctime()
+    print("CPU time: % .1f s, wall time: %.1f s"%(process_time() - tc, perf_counter() - tt))
 
-print("CPU time: % .1f s, wall time: %.1f s"%(process_time() - tc, perf_counter() - tt))
+if __name__=='__main__':
+    main(sys.argv)
